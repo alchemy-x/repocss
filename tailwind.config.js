93128fe0d929db25863defe7d654c8a91450143d
alchemy-x <134489593+alchemy-x@users.noreply.github.com>
2023-11-24 00:45:53 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontSize: {
+      mammoth:  '8rem',
+      },
+       colors: {
+      primary:  '#687EFF',
+      achena: {
+        100:'#40b3cc',
+        200:'#982176',
+        300:'#d1d7d9',
+        
+      }
+  
+      },
+      fontFamily:{
+        ace :['Montserrat, sans-serif'
+]
+      }
+    }
+  }
+}
+
